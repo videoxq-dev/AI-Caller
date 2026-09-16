9f14b91ddc35c3eb7513ea93c4c93f5727417c77
videoxq-dev <134068462+videoxq-dev@users.noreply.github.com>
2026-09-17 00:07:21 +0100

    Load Web Chat setup styles

--- app/setup/communication/layout.tsx
+++ app/setup/communication/layout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode } from "react";
 import "./sms.css";
 import "./whatsapp.css";
+import "./webchat.css";
 
 export default function CommunicationLayout({ children }: { children: ReactNode }) {
   return children;
