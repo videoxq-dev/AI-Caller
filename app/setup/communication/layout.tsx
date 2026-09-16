4b01b5483c5d5081ad2e50c4097408e3556c8d8e
videoxq-dev <134068462+videoxq-dev@users.noreply.github.com>
2026-09-16 23:50:04 +0100

    Load WhatsApp setup styles

--- app/setup/communication/layout.tsx
+++ app/setup/communication/layout.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from "react";
 import "./sms.css";
+import "./whatsapp.css";
 
 export default function CommunicationLayout({ children }: { children: ReactNode }) {
   return children;
