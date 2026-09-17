37c7385aa2e5d443be2119604b51d7aa88a7b504
videoxq-dev <134068462+videoxq-dev@users.noreply.github.com>
2026-09-17 12:47:32 +0100

    Load core contact drawer styles

--- app/contacts/page.tsx
+++ app/contacts/page.tsx
@@ -1,6 +1,7 @@
 import { ContactsDataPage } from "@/components/core-domain/contacts-data-page";
 import "../dashboard/dashboard.css";
 import "./contacts.css";
+import "./core-domain.css";
 
 export default function ContactsPage() {
   return <ContactsDataPage />;
