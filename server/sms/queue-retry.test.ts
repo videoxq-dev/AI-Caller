cf362f2237237b756f56c19a68b764a64a11db8a
videoxq-dev <134068462+videoxq-dev@users.noreply.github.com>
2026-09-18 19:14:51 +0100

    Cover managed SMS readiness in runtime tests

--- server/sms/queue-retry.test.ts
+++ server/sms/queue-retry.test.ts
@@ -41,6 +41,7 @@ describe("SMS queue retry behavior", () => {
       integrationId: "22222222-2222-4222-8222-222222222222",
       senderNumber: "+12025550200",
       serviceStatus: null,
+      messagingReadiness: null,
       provider,
     };
   });
