3923b3ab2ac8b03a7de8b0c6aaf8b60f1f8809f2
videoxq-dev <134068462+videoxq-dev@users.noreply.github.com>
2026-09-22 20:47:39 +0100

    Guard Realtime registry membership at runtime

--- server/orchestrator/action-registry.ts
+++ server/orchestrator/action-registry.ts
@@ -88,6 +88,10 @@ const realtimeToolCapabilityRules = {
 
 export type RealtimeBusinessToolName = keyof typeof realtimeToolCapabilityRules;
 
+export function isRealtimeBusinessToolName(name: string): name is RealtimeBusinessToolName {
+  return Object.prototype.hasOwnProperty.call(realtimeToolCapabilityRules, name);
+}
+
 export function registeredAgentAction(name: RegisteredAgentActionName) {
   return agentActionRegistry[name];
 }
